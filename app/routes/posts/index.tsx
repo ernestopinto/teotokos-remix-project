e54a8160fafea3284337dc89dfe8157a4f3565f2
Ernesto Pinto <ernesto.pinto@devoteam.com>
2022-12-28 18:46:20 +0000

    Tailwind

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Posts() {
+    return (
+        <main>
+            <h1 className="text-3xl font-bold underline">
+               Posts com tailwind
+            </h1>
+        </main>
+    );
+}
